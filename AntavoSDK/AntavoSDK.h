3ae418fa0a0d5a0d4ea0ef97260d1edf89e344d0
feketezsolt <zsolt@antavo.com>
2016-11-08 19:33:32 +0100

    Initial Commit

---
+++ AntavoSDK/AntavoSDK.h
@@ -0,0 +1,19 @@
+//
+//  AntavoSDK.h
+//  AntavoSDK
+//
+//  Created by Fekete Zsolt on 11/8/16.
+//  Copyright © 2016 Fekete Zsolt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AntavoSDK.
+FOUNDATION_EXPORT double AntavoSDKVersionNumber;
+
+//! Project version string for AntavoSDK.
+FOUNDATION_EXPORT const unsigned char AntavoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AntavoSDK/PublicHeader.h>
+
+
